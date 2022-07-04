4e5ca7465d9933ccbca0978769810326930507a3
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2022-07-04 14:20:01 +0700

    Chapter 4: Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    base_title = t "base_title"
+    page_title.blank? ? base_title : page_title + " | " + base_title
+  end
 end
